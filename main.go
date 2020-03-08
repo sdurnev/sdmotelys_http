49a9e5e5fa6c508cf2047bb271594213abb52a4a
svdu <sdurnev@gmail.com>
2020-03-08 11:06:21 +0600

    Init 1

--- main.go
+++ main.go
@@ -1,113 +1,120 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"encoding/xml"
+	"flag"
 	"fmt"
+	"github.com/paulrosania/go-charset/charset"
+	_ "github.com/paulrosania/go-charset/charset"
+	_ "github.com/paulrosania/go-charset/data"
 )
 
 type Result struct {
 	XMLName  xml.Name `xml:"Result" json:"-"`
-	U12      string   `xml:"U12" json:"U12"`
-	P        string   `xml:"P" json:"P"`
-	PD       string   `xml:"PD" json:"PD"`
-	F        string   `xml:"F" json:"F"`
-	FP       string   `xml:"FP" json:"FP"`
-	FPT      string   `xml:"FPT" json:"FPT"`
-	UB       string   `xml:"UB" json:"UB"`
-	U23      string   `xml:"U23" json:"U23"`
-	U31      string   `xml:"U31" json:"U31"`
-	V1       string   `xml:"V1" json:"V1"`
-	V2       string   `xml:"V2" json:"V2"`
-	V3       string   `xml:"V3" json:"V3"`
-	I1       string   `xml:"I1" json:"I1"`
-	I2       string   `xml:"I2" json:"I2"`
-	I3       string   `xml:"I3" json:"I3"`
-	IN       string   `xml:"IN" json:"IN"`
-	Q        string   `xml:"Q" json:"Q"`
-	S        string   `xml:"S" json:"S"`
-	CPP      string   `xml:"CPP" json:"CPP"`
-	CPT      string   `xml:"CPT" json:"CPT"`
-	CQP      string   `xml:"CQP" json:"CQP"`
-	CQT      string   `xml:"CQT" json:"CQT"`
-	MOT1     string   `xml:"MOT1" json:"MOT1"`
-	MOT2     string   `xml:"MOT2" json:"MOT2"`
-	MOT3     string   `xml:"MOT3" json:"MOT3"`
-	MOT4     string   `xml:"MOT4" json:"MOT4"`
-	MOT5     string   `xml:"MOT5" json:"MOT5"`
-	IB       string   `xml:"IB" json:"IB"`
-	PH       string   `xml:"PH" json:"PH"`
-	TH       string   `xml:"TH" json:"TH"`
-	WTH      string   `xml:"WTH" json:"WTH"`
-	NF       string   `xml:"NF" json:"NF"`
-	RPM      string   `xml:"RPM" json:"RPM"`
+	U12      float64  `xml:"U12" json:"GENS_L1-L2"`
+	P        float64  `xml:"P" json:"P"`
+	PD       float64  `xml:"PD" json:"PD"`
+	F        float64  `xml:"F" json:"GENS_FREQ"`
+	FP       float64  `xml:"FP" json:"FP"`
+	FPT      float64  `xml:"FPT" json:"FPT"`
+	UB       float64  `xml:"UB" json:"GENS_BAT_U"`
+	U23      float64  `xml:"U23" json:"GENS_L2-L3"`
+	U31      float64  `xml:"U31" json:"GENS_L3-L1"`
+	V1       float64  `xml:"V1" json:"GENS_L1-N"`
+	V2       float64  `xml:"V2" json:"GENS_L2-N"`
+	V3       float64  `xml:"V3" json:"GENS_L3-N"`
+	I1       float64  `xml:"I1" json:"GENS_CUR_A_L1"`
+	I2       float64  `xml:"I2" json:"GENS_CUR_A_L2"`
+	I3       float64  `xml:"I3" json:"GENS_CUR_A_L3"`
+	IN       float64  `xml:"IN" json:"IN"`
+	Q        float64  `xml:"Q" json:"Q"`
+	S        float64  `xml:"S" json:"S"`
+	CPP      float64  `xml:"CPP" json:"GENS_ACT_ENER_P"`
+	CPT      float64  `xml:"CPT" json:"GENS_ACT_ENER_T"`
+	CQP      float64  `xml:"CQP" json:"GENS_RACT_ENER_P"`
+	CQT      float64  `xml:"CQT" json:"GENS_RACT_ENER_T"`
+	MOT1     float64  `xml:"MOT1" json:"MOT1"`
+	MOT2     float64  `xml:"MOT2" json:"MOT2"`
+	MOT3     float64  `xml:"MOT3" json:"MOT3"`
+	MOT4     float64  `xml:"MOT4" json:"MOT4"`
+	MOT5     float64  `xml:"MOT5" json:"MOT5"`
+	IB       float64  `xml:"IB" json:"IB"`
+	PH       float64  `xml:"PH" json:"GENS_OIL_P"`
+	TH       string   `xml:"TH" json:"GENS_OIL_T"`
+	WTH      float64  `xml:"WTH" json:"GENS_TEN_D"`
+	NF       float64  `xml:"NF" json:"NF"`
+	RPM      float64  `xml:"RPM" json:"GENS_RPM"`
 	CTP      string   `xml:"CTP" json:"CTP"`
 	CTT      string   `xml:"CTT" json:"CTT"`
-	WTB      string   `xml:"WTB" json:"WTB"`
-	OVERRIDE string   `xml:"OVERRIDE" json:"OVERRIDE"`
-}
-
-type Data struct {
-	XMLName    xml.Name `xml:"data" json:"-"`
-	PersonList []Person `xml:"person" json:"people"`
-}
-
-type Person struct {
-	XMLName   xml.Name `xml:"person" json:"-"`
-	Firstname string   `xml:"firstname" json:"firstname"`
-	Lastname  string   `xml:"lastname" json:"lastname"`
-	Address   *Address `xml:"address" json:"address,omitempty"`
-}
-
-type Address struct {
-	City  string `xml:"city" json:"city,omitempty"`
-	State string `xml:"state" json:"state,omitempty"`
+	WTB      float64  `xml:"WTB" json:"WTB"`
+	OVERRIDE float64  `xml:"OVERRIDE" json:"OVERRIDE"`
 }
 
 func main() {
-	//rawXmlData := `
-	rawXmlData := `<?xml version="1.0" encoding="UTF-8" ?>
-<Result DateTime="6/3/2020 10:05">
-<U12>399.8</U12>
-<P>0.0</P>
-<PD>100</PD>
-<F>52.19</F>
-<FP>0.0</FP>
-<FPT>0</FPT>
-<UB>14.7</UB>
-<U23>403.0</U23>
-<U31>400.7</U31>
-<V1>230.5</V1>
-<V2>231.8</V2>
-<V3>232.4</V3>
-<I1>0.0</I1>
-<I2>0.0</I2>
-<I3>0.0</I3>
-<IN>0.0</IN>
-<Q>0.0</Q>
-<S>0.0</S>
-<CPP>2448</CPP>
-<CPT>2448</CPT>
-<CQP>0</CQP>
-<CQT>0</CQT>
-<MOT1>0</MOT1>
-<MOT2>0</MOT2>
-<MOT3>0</MOT3>
-<MOT4>0</MOT4>
-<MOT5>0</MOT5>
-<IB>0</IB>
-<PH>4.7</PH>
-<TH>----</TH>
-<WTH>67</WTH>
-<NF>100</NF>
-<RPM>1565</RPM>
-<CTP>00:55:25</CTP>
-<CTT>581:30:39</CTT>
-<WTB>0</WTB>
-<OVERRIDE>0</OVERRIDE>
-</Result>`
-	var data Result
-	xml.Unmarshal([]byte(rawXmlData), &data)
-	jsonData, _ := json.Marshal(data)
+	var rawXmlData string
+
+	StringData := flag.String("data", "0", "a string")
+	flag.Parse()
+
+	if *StringData == "0" {
+		rawXmlData = `<?xml version="1.0" encoding="ISO-8859-15" ?>
+		<Result DateTime="6/3/2020 10:05">
+		<U12>399.8</U12>
+		<P>0.0</P>
+		<PD>100</PD>
+		<F>52.19</F>
+		<FP>0.0</FP>
+		<FPT>0</FPT>
+		<UB>14.7</UB>
+		<U23>403.0</U23>
+		<U31>400.7</U31>
+		<V1>230.5</V1>
+		<V2>231.8</V2>
+		<V3>232.4</V3>
+		<I1>0.0</I1>
+		<I2>0.0</I2>
+		<I3>0.0</I3>
+		<IN>0.0</IN>
+		<Q>0.0</Q>
+		<S>0.0</S>
+		<CPP>2448</CPP>
+		<CPT>2448</CPT>
+		<CQP>0</CQP>
+		<CQT>0</CQT>
+		<MOT1>0</MOT1>
+		<MOT2>0</MOT2>
+		<MOT3>0</MOT3>
+		<MOT4>0</MOT4>
+		<MOT5>0</MOT5>
+		<IB>0</IB>
+		<PH>4.7</PH>
+		<TH>----</TH>
+		<WTH>67</WTH>
+		<NF>100</NF>
+		<RPM>1565</RPM>
+		<CTP>00:55:25</CTP>
+		<CTT>581:30:39</CTT>
+		<WTB>0</WTB>
+		<OVERRIDE>0</OVERRIDE>
+		</Result>`
+	} else {
+		rawXmlData = *StringData
+	}
+
+	var feed Result
+
+	reader := bytes.NewReader([]byte(rawXmlData))
+	decoder := xml.NewDecoder(reader)
+	decoder.CharsetReader = charset.NewReader
+	err := decoder.Decode(&feed)
+	if err != nil {
+		fmt.Println("decoder error:", err)
+	}
+	//fmt.Println(feed)
+
+	xml.Unmarshal([]byte(rawXmlData), &feed)
+	jsonData, _ := json.Marshal(feed)
 	fmt.Println(string(jsonData))
 }
--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-URL="localhost:1123"
+URL="localhost:1111"
 
-XML="$(curl 'http://'$URL'/WebTelys?Request=Mesure' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:73.0) Gecko/20100101 Firefox/73.0' -H 'Accept: */*' -H 'Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3' --compressed -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' -H 'Referer: http://'$URL'/Conduit.asp' -H 'Cookie: Langue=En')"
+XML="$(curl 'http://localhost:1111/WebTelys?Request=Mesure' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:73.0) Gecko/20100101 Firefox/73.0' -H 'Accept: */*' -H 'Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3' --compressed -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' -H 'Referer: http://localhost:1111/Conduit.asp' -H 'Cookie: Langue=En')"
 
 #XML="$(curl 'http://localhost:1123/WebTelys?Request=Mesure' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:73.0) Gecko/20100101 Firefox/73.0' -H 'Accept: */*' -H 'Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3' --compressed -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' -H 'Referer: http://localhost:1123/Conduit.asp' -H 'Cookie: Langue=En')"
 
